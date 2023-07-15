1b4579137e38a0cf7853e6678a3602277660899a
Nguyen Duy Anh Quoc <quoca371@gmail.com>
2023-07-15 07:26:59 +0700

    change contract

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Login = (props) => {
     return (
-        <div className="login-container">
+        <div className="end-container">
             <h1 className="welcome-message">Welcome to Voting Dapp</h1>
             <button className="login-button" onClick = {props.connectWallet}>Login Metamask</button>
         </div>
